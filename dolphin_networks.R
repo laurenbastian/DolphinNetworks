caa5a13a7824a36526e0fe044d432f4bd5f6d95b
laurenbastian <laurenlbastian@gmail.com>
2021-06-04 10:33:14 -0500

    Corrected Stub Selection in config.model Function

--- dolphin_networks.R
+++ dolphin_networks.R
@@ -36,7 +36,7 @@ for (i in 1:length(names))
 ## NOTE: THIS ALGORITHM MAY PRODUCE MATRICES WITH SELF-LOOPS
 ## AND MULTI-EDGES!
 
-configuration.model = function(deg.seq)
+config.model = function(deg.seq)
 {
   ## invalid if the sum of the degree sequence is odd
   if(sum(deg.seq) %% 2 == 1)
@@ -59,19 +59,48 @@ configuration.model = function(deg.seq)
   
   while(num.stubs > 0)
   {
+    print("START")
+    print(stubs.vec)
+    
     ## randomly select two stubs
-    stubs = sample(x = stubs.vec, size = 2)
-  
+    stubs = sample(stubs.vec, 2)
+    
+    print(stubs)
+    
     ## connect the two stubs to form an edge
     adj.mat[stubs[1], stubs[2]] = adj.mat[stubs[1], stubs[2]] + 1
     adj.mat[stubs[2], stubs[1]] = adj.mat[stubs[2], stubs[1]] + 1
   
     ## remove both stubs from stubs.vec
-    stubs.vec = stubs.vec[-stubs[1]]
-    stubs.vec = stubs.vec[-stubs[2]]
+    ## find the index of stub 1 and remove it
+    stub.idx = 0
+    for (i in 1:num.stubs)
+    {
+      if(stubs[1] == stubs.vec[i])
+      {
+        stub.idx = i
+        break
+      }
+    }
+    stubs.vec = stubs.vec[-stub.idx]
+    
+    ## find the index of stub 2 and remove it
+    stub.idx = 0
+    for (i in 1:num.stubs)
+    {
+      if(stubs[2] == stubs.vec[i])
+      {
+        stub.idx = i
+        break
+      }
+    }
+    stubs.vec = stubs.vec[-stub.idx]
     
     ## decrement number of stubs
     num.stubs = num.stubs - 2
+    
+    print("END")
+    print(stubs.vec)
   }
   
   return (adj.mat)
@@ -81,26 +110,22 @@ configuration.model = function(deg.seq)
 ## following a given degree sequence
 ## without self-loops or multi-edges
 ## returns a list of valid networks
-configure.valid.networks = function(deg.seq, reps)
+config.valid.networks = function(deg.seq, reps)
 {
-  i = 1
+  ## a list of matrices
   sim.matrices = list()
+  i = 1
   while(i <= reps)
   {
     ## generate a random matrix
-    rand.mat = configuration.model(deg.seq)
+    rand.mat = config.model(deg.seq)
     
     ##check if the matrix is valid
+    ##search for multi-edges
     is.valid.mat = TRUE
     for (j in 1:nrow(rand.mat))
     {
-      ##if there is a self-loop, matrix is invalid
-      if(rand.mat[j, j] != 0)
-      {
-       is.valid.mat = FALSE
-       break
-      #}
-      for (k in 1:ncol(rand.mat))
+      for (k in 1:nrow(rand.mat))
       {
         ## if there is a multi-edge, matrix is invalid
         if(rand.mat[j, k] > 1)
@@ -110,6 +135,17 @@ configure.valid.networks = function(deg.seq, reps)
         }
       }
     }
+    
+    ##check for self-loops
+    for (j in 1:nrow(rand.mat))
+    {
+      if(rand.mat[j, j] != 0)
+      {
+        is.valid.mat = FALSE
+        break
+      }
+    }
+    
     ## if the matrix is valid, add it to the list and increment i
     if (is.valid.mat)
     {
@@ -117,90 +153,8 @@ configure.valid.networks = function(deg.seq, reps)
       i = i + 1
     }
   }
-  
+  ## return a list of adjacency matrices
   return (sim.matrices)
 }
 
-configure.valid.networks(dolphin.deg.seq, 1)
-
-configure.valid.networks(test.deg.seq, 5)
-
-## checking uniformity
-##THIS IS VERY MUCH NOT UNIFORM SOMETHING IS UP
-##LIKELY AN ERROR IN THE VALID NETWORKS FUNCTION??
-a = matrix(data = c(0,0,0,1,0,0,1,1,0,1,0,1,1,1,1,0), nrow = 4)
-b = matrix(data = c(0,0,1,1,0,0,0,1,1,0,0,1,1,1,1,0), nrow = 4)
-c = matrix(data = c(0,1,0,1,1,0,0,1,0,0,0,1,1,1,1,0), nrow = 4)
-d = matrix(data = c(0,1,1,1,1,0,1,0,1,1,0,0,1,0,0,0), nrow = 4)
-e = matrix(data = c(0,1,1,1,1,0,0,1,1,0,0,0,1,1,0,0), nrow = 4)
-f = matrix(data = c(0,1,1,1,1,0,0,0,1,0,0,1,1,0,1,0), nrow = 4)
-g = matrix(data = c(0,1,1,0,1,0,1,1,1,1,0,0,0,1,0,0), nrow = 4)
-h = matrix(data = c(0,1,0,1,1,0,1,1,0,1,0,0,1,1,0,0), nrow = 4)
-i = matrix(data = c(0,1,0,0,1,0,1,1,0,1,0,1,0,1,1,0), nrow = 4)
-j = matrix(data = c(0,1,1,0,1,0,1,0,1,1,0,1,0,0,1,0), nrow = 4)
-k = matrix(data = c(0,0,1,1,0,0,1,0,1,1,0,1,1,0,1,0), nrow = 4)
-l = matrix(data = c(0,0,1,0,0,0,1,1,1,1,0,1,0,1,1,0), nrow = 4)
-
-reps = 1000 
-
-test.nets = configure.valid.networks(test.deg.seq, reps)
-
-test.freq = numeric(reps)
-
-for(i in 1:reps)
-{
-  if(identical(test.nets[[i]], a))
-  {
-    test.freq[i] = 1
-  }
-  else if(identical(test.nets[[i]], b))
-  {
-    test.freq[i] = 2
-  }
-  else if(identical(test.nets[[i]], c))
-  {
-    test.freq[i] = 3
-  }
-  else if(identical(test.nets[[i]], d))
-  {
-    test.freq[i] = 4
-  }
-  else if(identical(test.nets[[i]], e))
-  {
-    test.freq[i] = 5
-  }
-  else if(identical(test.nets[[i]], f))
-  {
-    test.freq[i] = 6
-  }
-  else if(identical(test.nets[[i]], g))
-  {
-    test.freq[i] = 7
-  }
-  else if(identical(test.nets[[i]], h))
-  {
-    test.freq[i] = 8
-  }
-  else if(identical(test.nets[[i]], i))
-  {
-    test.freq[i] = 9
-  }
-  else if(identical(test.nets[[i]], j))
-  {
-    test.freq[i] = 10
-  }
-  else if(identical(test.nets[[i]], k))
-  {
-    test.freq[i] = 11
-  }
-  else if(identical(test.nets[[i]], l))
-  {
-    test.freq[i] = 12
-  }
-  else
-  {
-    test.freq[i] = 13
-  }
-}
-hist(test.freq)
 
