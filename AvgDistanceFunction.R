8a836b0dda9ffdf65bad0884ce3552e002d30128
laurenbastian <laurenlbastian@gmail.com>
2021-06-10 15:16:33 -0500

    Updated output, average efficiency function

--- AvgDistanceFunction.R
+++ AvgDistanceFunction.R
@@ -1,7 +1,5 @@
-##NOTE: ALL FUNCTIONS ASSUME THE GRAPH IS CONNECTED
-
 ##This is a function that will find the shortest path
-##between a start node and any two nodes on a graph
+##between a start node and any other two nodes on a graph
 ##adj.mat: an adjacency matrix representing the graph
 ##start: the initial point on the graph
 ##return a list of lengths from the start node to all other nodes
@@ -46,7 +44,7 @@ single.source = function(adj.mat, start)
     ##add unvisited neighbors of the vertex to the queue
     for (i in 1:ncol(adj.mat))
     {
-      if(adj.mat[cur.df$To, i] == 1 && !visited[i])
+      if((adj.mat[cur.df$To, i] == 1) && (!visited[i]))
       {
         queue.df = rbind(queue.df, data.frame("Source" = start, "To" = i, "Prev" = cur.df$To, "Cost" = cur.df$Cost + 1))
       }
@@ -55,6 +53,25 @@ single.source = function(adj.mat, start)
     ##remove the first node from the queue
     queue.df = queue.df[-1,]
   }
+  
+  ##add infinite distances for unconnected nodes
+  ##if there is a node that is not reached by the algorithm, 
+  ##its distance is infinite
+  for (i in 1:ncol(adj.mat))
+  {
+    if (i == start)
+    {
+      next
+    }
+    
+    if (!(i %in% output.df$To))
+    {
+      output.df = rbind(output.df, data.frame("Source" = start, "To" = i, "Prev" = NA, "Cost" = Inf))
+    }
+  }
+  
+  ##order the output dataframe by the end node "To"
+  output.df = output.df[order(output.df$To), ]
 
   return (output.df)
 }
@@ -87,13 +104,29 @@ avg.distance = function(adj.mat)
 }
 
 
+##A function that calculates the average efficiency of a network 
+avg.efficiency = function(adj.mat)
+{
+  all.distances = data.frame("Source" = c(), "To" = c(), "Prev" = c(), "Cost" = c())
+  for (i in 1:ncol(adj.mat))
+  {
+    all.distances = rbind(all.distances, single.source(adj.mat, i))
+  }
+
+  return ((1 / ((ncol(adj.mat)) * (ncol(adj.mat) -1)) * ((sum(all.distances$Cost ^ -1)) / 2)))
+}
+
+
 ##Verify correctness
 library(igraph)
 mytest = c(3,3,3,2,1,1,1)
 mytest = config.valid.networks(mytest, 1)
 mytest = mytest[[1]]
 plot(graph_from_adjacency_matrix(mytest, mode = "undirected"))
-single.source(mytest, 2)
+start.time = Sys.time()
+mydistances = single.source(mytest, 2)
+end.time = Sys.time()
 diameter(mytest)
 avg.distance(mytest)
+avg.efficiency(mytest)
 
